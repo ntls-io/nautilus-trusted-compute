319fc43ac236e26dc47e70a2b1750cf493a08680
Pi Delport <pi@ntls.io>
2022-04-04 06:16:46 +0200

    feat(ntc-data-packages): add Metadata::from_json_bytes

--- rust-workspace/crates/ntc-data-packages/src/data_packages/common.rs
+++ rust-workspace/crates/ntc-data-packages/src/data_packages/common.rs
@@ -1,5 +1,7 @@
 //! The common, serialised representation of data packages.
 
+use serde::{Deserialize, Serialize};
+
 /// A data package associates a [`Dataset`] with some descriptive [`Metadata`].
 pub struct DataPackage {
     pub metadata: Metadata,
@@ -7,12 +9,20 @@ pub struct DataPackage {
 }
 
 /// Metadata that describes some [`Dataset`].
+#[derive(Debug)] // core
+#[derive(Serialize, Deserialize)] // serde
 pub struct Metadata {
     pub name: String,
     pub version: String,
     pub creator: String,
     pub timestamp: String,
     pub description: String,
+}
+
+impl Metadata {
+    pub fn from_json_bytes(value: &[u8]) -> Result<Self, serde_json::Error> {
+        serde_json::from_slice(value)
+    }
 }
 
 /// A serialised dataset contains some serialised data, and some serialised schema describing it.
