85456d15eb642a5a4da7789eec0337be6ec548d7
Bill Guo <bill@ntls.io>
2022-05-12 13:12:51 +0200

    feat(ntc-oracle-node): add config crate

--- rust-workspace/crates/ntc-oracle-node/src/main.rs
+++ rust-workspace/crates/ntc-oracle-node/src/main.rs
@@ -1,21 +1,23 @@
-//Standalone binary that exposes HTTP interface that reads information from Algorand and signs the resulting authorization data
+//! Standalone binary that exposes HTTP interface that reads information from Algorand and
+//! signs the resulting authorization data
+
+mod settings;
 
 use algonaut::core::Round;
 use algonaut::indexer::v2::Indexer;
 use algonaut_client::Headers;
-use dotenv::dotenv;
+use settings::Settings;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // load variables in .env
-    dotenv().ok();
+    // load configuration settings
+    let settings = Settings::new().unwrap();
+    println!("{:?}", settings);
 
-    let indexer_url = &dotenv::var("INDEXER_URL").unwrap();
-    let indexer_api_token = &dotenv::var("INDEXER_API_TOKEN").unwrap();
-
-    let indexer_header: Headers = vec![("X-API-Key", indexer_api_token)];
-    let indexer = Indexer::with_headers(indexer_url, indexer_header).unwrap();
+    let indexer_header: Headers = vec![("X-API-Key", settings.purestake.indexer_token.as_str())];
+    let indexer =
+        Indexer::with_headers(settings.purestake.indexer_url.as_str(), indexer_header).unwrap();
 
     let block = indexer.block(Round(6)).await.unwrap();
     println!("Previous Block Hash: {}", block.previous_block_hash);
