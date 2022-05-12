ab940881906a92f33fc307e1633207f53fec793c
Bill Guo <bill@ntls.io>
2022-05-12 13:12:51 +0200

    feat(ntc-oracle-node): add config crate

---
+++ rust-workspace/crates/ntc-oracle-node/src/settings.rs
@@ -0,0 +1,37 @@
+// https://github.com/mehcode/config-rs/blob/master/examples/hierarchical-env/settings.rs
+
+use config::{Config, ConfigError, File};
+use serde_derive::Deserialize;
+use std::env;
+
+#[derive(Debug, Deserialize)]
+pub struct Purestake {
+    pub indexer_url: String,
+    pub indexer_token: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub purestake: Purestake,
+}
+
+impl Settings {
+    pub fn new() -> Result<Self, ConfigError> {
+        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
+
+        let s = Config::builder()
+            // Start off by merging in the "default" configuration file
+            .add_source(File::with_name("config/default.toml"))
+            // Add in the current environment file
+            // Default to 'development' env
+            // Note that this file is _optional_
+            .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
+            // Add in a local configuration file
+            // This file shouldn't be checked in to git
+            .add_source(File::with_name("config/local.toml").required(false))
+            .build()?;
+
+        // You can deserialize (and thus freeze) the entire configuration as
+        s.try_deserialize()
+    }
+}
