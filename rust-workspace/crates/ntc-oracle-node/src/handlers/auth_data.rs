0e1f5a73a476dcd7b542f53fe23e29cae9e22391
Bill Guo <bill@ntls.io>
2022-06-13 14:19:33 +0200

    feat(ntc-oracle-node): add anyhow error handling

--- rust-workspace/crates/ntc-oracle-node/src/handlers/auth_data.rs
+++ rust-workspace/crates/ntc-oracle-node/src/handlers/auth_data.rs
@@ -1,18 +1,16 @@
-use std::str::FromStr;
+use std::io::Error;
+use std::path::Path;
 
-use algonaut::core::Address;
 use algonaut::crypto::Signature;
 use algonaut::indexer::v2::Indexer;
 use algonaut::model::indexer::v2::{QueryAssetTransaction, QueryAssetsInfo};
 use algonaut_client::Headers;
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use anyhow::Context;
 use axum::Json;
-use ring_compat::ring::rand;
-use ring_compat::ring::signature::Ed25519KeyPair;
 use ring_compat::signature::ed25519::SigningKey;
 use serde::{Deserialize, Serialize};
 
+use crate::errors::AnyhowErrorResponse;
 use crate::settings::Settings;
 use crate::signer::AuthDataSigner;
 
@@ -26,15 +24,15 @@ pub(crate) struct AuthInput {
 /// Response of [`auth_data`]
 #[derive(Serialize)]
 pub(crate) struct SignedAuthData {
-    auth_data: AuthData,
+    auth_data: Vec<u8>,
     signature: Signature,
 }
 
 #[derive(Serialize)]
 pub(crate) struct AuthData {
     redeemed: bool,
-    drt_creator: Address,
-    drt_redeemer: Address,
+    drt_creator: String,
+    drt_redeemer: String,
     binary: Vec<u8>,
     data_package: Vec<u8>,
     binary_url: String,
@@ -49,87 +47,98 @@ struct DrtNote {
     data_url: String,
 }
 
-pub(crate) async fn auth_data(Json(auth_input): Json<AuthInput>) -> impl IntoResponse {
-    let auth_data = get_auth_data(auth_input).await;
-    let signed_auth_data = sign_auth_data(auth_data);
-    (StatusCode::OK, Json(signed_auth_data))
+/// Query authorization data for redeemed DRT.
+///
+/// This will retrieve DRT information from Algorand blockchain, and return signed response.
+pub(crate) async fn auth_data(
+    Json(auth_input): Json<AuthInput>,
+) -> Result<Json<SignedAuthData>, AnyhowErrorResponse> {
+    // Read key from PKCS#8 der file
+    let pkcs8_bytes = read_file(Path::new("/tmp/pkcs8.der"))?;
+    let signing_key = SigningKey::from_pkcs8(pkcs8_bytes.as_ref())?;
+
+    let auth_data = get_auth_data(auth_input).await?;
+    let auth_data_bytes = serde_json::to_vec(&auth_data)?;
+    let signed_auth_data = sign_auth_data(auth_data_bytes, signing_key);
+    Ok(Json(signed_auth_data))
 }
 
-fn sign_auth_data(auth_data: AuthData) -> SignedAuthData {
-    // Generate a key pair in PKCS#8 (v2) format.
-    let rng = rand::SystemRandom::new();
-    let pkcs8_bytes = Ed25519KeyPair::generate_pkcs8(&rng).unwrap();
-
-    // TODO:    Normally the application would store the PKCS#8 file persistently.
-    //          Later it would read the PKCS#8 file from persistent storage to use it.
-
-    pub(crate) type RingAuthDataSigner = AuthDataSigner<SigningKey>;
-    let signing_key = SigningKey::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
+fn sign_auth_data(auth_data: Vec<u8>, signing_key: SigningKey) -> SignedAuthData {
+    type RingAuthDataSigner = AuthDataSigner<SigningKey>;
 
     let signer = RingAuthDataSigner { signing_key };
     let signature = signer.sign(&auth_data);
     println!("Signature: {}", signature);
 
+    // convert ed25519::Signature to algonaut_crypto::Signature (Ed25519)
+    let signature = Signature(signature.to_bytes());
+
     SignedAuthData {
         auth_data,
-        signature: Signature(signature.to_bytes()),
+        signature,
     }
 }
 
-async fn get_auth_data(auth_input: AuthInput) -> AuthData {
-    let indexer = init_purestake_indexer_client();
+async fn get_auth_data(auth_input: AuthInput) -> Result<AuthData, AnyhowErrorResponse> {
+    let indexer = init_purestake_indexer_client()?;
 
     let txn_id = auth_input.txn_id;
     let asset_id = auth_input.asset_id;
 
-    let transaction_info = indexer
-        .transaction_info(&txn_id)
-        .await
-        .expect("Incorrect Transaction ID");
+    let transaction_info = indexer.transaction_info(&txn_id).await?;
 
     let asset_info = indexer
         .assets_info(asset_id, &QueryAssetsInfo::default())
-        .await
-        .expect("Incorrect Asset ID");
+        .await?;
 
     // `redeemed` - check txn receiver is same as reserve address of DRT
     let txn_receiver = transaction_info
         .transaction
         .asset_transfer_transaction
-        .unwrap()
+        .context("Fail to get transaction information")?
         .receiver;
     println!("Transaction receiver: {}", txn_receiver);
-    let asset_reserve_addr = asset_info.asset.params.reserve.unwrap();
+    let asset_reserve_addr = asset_info
+        .asset
+        .params
+        .reserve
+        .context("Fail to get asset reserve address")?;
     println!("Reserve address: {}", asset_reserve_addr);
     let redeemed = txn_receiver == asset_reserve_addr;
 
     // `drt_creator` - management address of DRT
-    let drt_creator = asset_info.asset.params.manager.unwrap();
+    let drt_creator = asset_info
+        .asset
+        .params
+        .manager
+        .context("Fail to get asset management address")?
+        .to_string();
     println!("Manager address: {}", drt_creator);
 
     // `drt_redeemer`
-    let txn_sender = transaction_info.transaction.sender;
-    println!("Transaction sender: {}", txn_sender);
-    let drt_redeemer = Address::from_str(txn_sender.as_str()).unwrap();
+    let drt_redeemer = transaction_info.transaction.sender;
+    println!("Transaction sender: {}", drt_redeemer);
 
     // Get information encoded in the DRT
     // Find first config transaction for DRT to get Note field
     let asset_transactions = indexer
         .asset_transactions(asset_id, &QueryAssetTransaction::default())
-        .await
-        .expect("Incorrect Asset Transactions ID");
+        .await?;
 
     let config_transaction = asset_transactions.transactions;
-    let note_base64 = config_transaction[0].clone().note.unwrap();
-    let note = base64::decode(note_base64).unwrap();
+    let note_base64 = config_transaction[0]
+        .clone()
+        .note
+        .context("Fail to get note of config transaction for DRT")?;
+    let note = base64::decode(note_base64)?;
     let DrtNote {
         binary,
         binary_url,
         data_package,
         data_url,
-    } = serde_json::from_slice(note.as_slice()).unwrap();
+    } = serde_json::from_slice(note.as_slice())?;
 
-    AuthData {
+    Ok(AuthData {
         redeemed,
         drt_creator,
         drt_redeemer,
@@ -137,16 +146,24 @@ async fn get_auth_data(auth_input: AuthInput) -> AuthData {
         data_package,
         binary_url,
         data_url,
-    }
+    })
 }
 
-fn init_purestake_indexer_client() -> Indexer {
+fn init_purestake_indexer_client() -> Result<Indexer, AnyhowErrorResponse> {
     // load PureStake configuration settings
-    let settings = Settings::new().unwrap();
+    let settings = Settings::new()?;
 
     // connect v2 indexer client using algonaut
     let indexer_header: Headers = vec![("X-API-Key", settings.purestake.indexer_token.as_str())];
-    let indexer = Indexer::with_headers(settings.purestake.indexer_url.as_str(), indexer_header)
-        .expect("Error connecting to PureStake v2 indexer");
-    indexer
+    let indexer = Indexer::with_headers(settings.purestake.indexer_url.as_str(), indexer_header)?;
+    Ok(indexer)
+}
+
+fn read_file(path: &Path) -> Result<Vec<u8>, Error> {
+    use std::io::Read;
+
+    let mut file = std::fs::File::open(path)?;
+    let mut contents: Vec<u8> = Vec::new();
+    file.read_to_end(&mut contents)?;
+    Ok(contents)
 }
--- rust-workspace/crates/ntc-oracle-node/src/signer.rs
+++ rust-workspace/crates/ntc-oracle-node/src/signer.rs
@@ -1,7 +1,5 @@
 use ed25519::signature::{Signer, Verifier};
 
-use crate::handlers::auth_data::AuthData;
-
 pub(crate) struct AuthDataSigner<S>
 where
     S: Signer<ed25519::Signature>,
@@ -13,12 +11,11 @@ impl<S> AuthDataSigner<S>
 where
     S: Signer<ed25519::Signature>,
 {
-    pub(crate) fn sign(&self, auth_data: &AuthData) -> ed25519::Signature {
+    pub(crate) fn sign(&self, auth_data: &[u8]) -> ed25519::Signature {
         // NOTE: use `try_sign` if you'd like to be able to handle
         // errors from external signing services/devices (e.g. HSM/KMS)
         // <https://docs.rs/signature/latest/signature/trait.Signer.html#tymethod.try_sign>
-        self.signing_key
-            .sign(serde_json::to_vec(&auth_data).unwrap().as_slice())
+        self.signing_key.sign(auth_data)
     }
 }
 #[allow(dead_code)]
@@ -33,12 +30,9 @@ where
     #[allow(dead_code)]
     pub(crate) fn verify(
         &self,
-        auth_data: &AuthData,
+        auth_data: &[u8],
         signature: &ed25519::Signature,
     ) -> Result<(), ed25519::Error> {
-        self.verify_key.verify(
-            serde_json::to_vec(&auth_data).unwrap().as_slice(),
-            signature,
-        )
+        self.verify_key.verify(auth_data, signature)
     }
 }
