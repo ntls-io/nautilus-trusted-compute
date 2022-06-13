25fce2d5d9117b90849b3e845aa7930a1ed074c6
Bill Guo <bill@ntls.io>
2022-06-13 16:07:39 +0200

    feat(ntc-oracle-node): use serde_with to base64 encode SignedAuthData response

--- rust-workspace/crates/ntc-oracle-node/src/settings.rs
+++ rust-workspace/crates/ntc-oracle-node/src/settings.rs
@@ -1,7 +1,5 @@
 // https://github.com/mehcode/config-rs/blob/master/examples/hierarchical-env/settings.rs
 
-use std::env;
-
 use config::{Config, ConfigError, File};
 use serde_derive::Deserialize;
 
@@ -18,15 +16,9 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
-
         let s = Config::builder()
             // Start off by merging in the "default" configuration file
             .add_source(File::with_name("config/default.toml"))
-            // Add in the current environment file
-            // Default to 'development' env
-            // Note that this file is _optional_
-            .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
             // Add in a local configuration file
             // This file shouldn't be checked in to git
             .add_source(File::with_name("config/local.toml").required(false))
