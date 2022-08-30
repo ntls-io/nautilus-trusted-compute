c6c73b15a68d19153aad414256f500adfcbdeff8
Bill Guo <bill@ntls.io>
2022-08-30 12:40:21 +0200

    refactor: remove Onfido operations and XRP related code

--- rust-sgx-workspace/crates/sgx-vault-impl/src/vault_operations/create_vault.rs
+++ rust-sgx-workspace/crates/sgx-vault-impl/src/vault_operations/create_vault.rs
@@ -1,16 +1,15 @@
 use std::prelude::v1::ToString;
 
 use crate::schema::actions::{CreateVault, CreateVaultResult};
-use crate::schema::entities::{AlgorandAccount, VaultDisplay, VaultStorable, XrplAccount};
+use crate::schema::entities::{AlgorandAccount, VaultDisplay, VaultStorable};
 use crate::vault_operations::store::save_new_vault;
 
 type Result = CreateVaultResult;
 
 pub fn create_vault(request: &CreateVault) -> Result {
     // TODO(Pi): Pull account / keypair creation into a separate operation.
-    //           For now, just generate both Algorand and XRP keypairs.
+    //           For now, just generate Algorand keypairs.
     let new_algorand_account = AlgorandAccount::generate();
-    let new_xrpl_account = XrplAccount::generate_default();
 
     let storable = VaultStorable {
         vault_id: new_algorand_account.address_base32(),
@@ -19,9 +18,6 @@ pub fn create_vault(request: &CreateVault) -> Result {
         phone_number: request.phone_number.clone(),
 
         algorand_account: new_algorand_account,
-        xrpl_account: new_xrpl_account,
-
-        onfido_check_result: None,
     };
     match save_new_vault(&storable) {
         Ok(()) => Result::Created(VaultDisplay::from(storable)),
--- rust-sgx-workspace/projects/sgx-vault-test/enclave/src/helpers/vault_store.rs
+++ rust-sgx-workspace/projects/sgx-vault-test/enclave/src/helpers/vault_store.rs
@@ -1,18 +1,9 @@
 use std::prelude::v1::ToString;
 
 use sgx_vault_impl::schema::actions;
-use sgx_vault_impl::schema::actions::{
-    CreateVaultResult,
-    LoadOnfidoCheck,
-    LoadOnfidoCheckResult,
-    OnfidoCheckResult,
-    SaveOnfidoCheck,
-    SaveOnfidoCheckResult,
-};
+use sgx_vault_impl::schema::actions::CreateVaultResult;
 use sgx_vault_impl::schema::entities::VaultDisplay;
 use sgx_vault_impl::vault_operations::create_vault::create_vault;
-use sgx_vault_impl::vault_operations::load_onfido_check::load_onfido_check;
-use sgx_vault_impl::vault_operations::save_onfido_check::save_onfido_check;
 
 pub fn create_test_vault() -> VaultDisplay {
     type Result = CreateVaultResult;
@@ -24,34 +15,6 @@ pub fn create_test_vault() -> VaultDisplay {
     };
     match create_vault(request) {
         Result::Created(created) => created,
-        otherwise => panic!("{:?}", otherwise),
-    }
-}
-
-pub fn create_test_check(existing: &VaultDisplay) -> OnfidoCheckResult {
-    let check = OnfidoCheckResult {
-        id: "stub id".to_string(),
-        href: "stub href".to_string(),
-        result: "stub result".to_string(),
-        sub_result: None,
-    };
-    match save_onfido_check(&SaveOnfidoCheck {
-        vault_id: existing.vault_id.clone(),
-        auth_pin: "123456".to_string(),
-        check: check.clone(),
-    }) {
-        SaveOnfidoCheckResult::Saved => {}
-        otherwise => panic!("{:?}", otherwise),
-    };
-    check
-}
-
-pub fn load_test_check(existing: &VaultDisplay) -> OnfidoCheckResult {
-    match load_onfido_check(&LoadOnfidoCheck {
-        vault_id: existing.vault_id.clone(),
-        auth_pin: "123456".to_string(),
-    }) {
-        LoadOnfidoCheckResult::Loaded(check) => check,
         otherwise => panic!("{:?}", otherwise),
     }
 }
