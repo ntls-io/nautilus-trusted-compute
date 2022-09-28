7c3a0dba9895290cdf46926c5477ae6b35af5229
binglekruger <bingkruger@hotmail.co.za>
2022-09-28 09:34:34 +0000

    feat: Create two APIs for data operations

---
+++ rust-sgx-workspace/crates/http-service-impl/src/resources/data_operation.rs
@@ -0,0 +1,20 @@
+// Upload data pool API //
+// Encryt from client to enclave - seal using enclave key - upload to Azure Blob (or Cosmos DB)
+// Data is uploaded in JSON format
+// Encrypt: Use SSL or TLS
+
+
+
+
+// Upload to Azure Blob storage or Cosmos DB - TBD //
+// https://github.com/Azure/azure-sdk-for-rust
+// https://crates.io/crates/azure_storage_blobs
+
+
+
+
+
+// Query data API //
+// Frontend must show which data packages the data creator has uploaded to the system
+// Option 1 - Query data service for list of data packages uploaded by the data creator
+// Option 2 - Lookup smart contracts associated with data creator's NTLS wallet
