adc70fbb0c6d410c512f6d86b48fda6ca221d0e3
binglekruger <bingkruger@hotmail.co.za>
2022-10-24 13:53:49 +0000

    refactor: Update log -> data

--- rust-workspace/projects/data-server/src/data_handlers/mod.rs
+++ rust-workspace/projects/data-server/src/data_handlers/mod.rs
@@ -5,39 +5,40 @@ use mongodb::{options::FindOptions, Client};
 use std::sync::Mutex;
 use chrono::prelude::*;
 use serde::Deserialize;
+use std::env;
 
 #[derive(Deserialize)]
 pub struct NewDataPool {
     pub id: String,
     pub poolName: String, //User defined pool name
-    pub sealedString: String, //Update data format to match sealed data
+    pub sealedData: String, //Update data format to match sealed data
 }
 
-// Name for Mongo DB (CosmosDB)
+// Mongo DB (CosmosDB) Name
 const MONGO_DB: &'static str = "ntc-data";
-// Name for Mongo Collection
-const MONGO_COLL_LOGS: &'static str = "datapools";
+// Mongo Collection Name
+const MONGO_COLLECTION: &'static str = "datapools";
 
 pub fn scoped_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::resource("/logs")
-            .route(web::get().to(get_logs))
-            .route(web::post().to(add_log)),
+        web::resource("/data")
+            .route(web::get().to(get_data))
+            .route(web::post().to(add_data)),
     );
 }
 
 // This function accepts application data
-async fn get_logs(data: web::Data<Mutex<Client>>) -> impl Responder {
-    let logs_collection = data
+async fn get_data(data: web::Data<Mutex<Client>>) -> impl Responder {
+    let data_collection = data
         .lock()
         .unwrap()
         .database(MONGO_DB)
-        .collection(MONGO_COLL_LOGS);
+        .collection(MONGO_COLLECTION);
 
-    let filter = doc! {};
+    let filter = doc! { };
     // Tweak find options to find specific user's data
     let find_options = FindOptions::builder().sort(doc! { "_id": -1}).build();
-    let mut cursor = logs_collection.find(filter, find_options).await.unwrap();
+    let mut cursor = data_collection.find(filter, find_options).await.unwrap();
 
     let mut results = Vec::new();
     while let Some(result) = cursor.next().await {
@@ -53,14 +54,14 @@ async fn get_logs(data: web::Data<Mutex<Client>>) -> impl Responder {
     HttpResponse::Ok().json(results)
 }
 
-async fn add_log(data: web::Data<Mutex<Client>>, new_log: web::Json<NewDataPool>) -> impl Responder {
-    let logs_collection = data
+async fn add_data(data: web::Data<Mutex<Client>>, new_pool: web::Json<NewDataPool>) -> impl Responder {
+    let data_collection = data
         .lock()
         .unwrap()
         .database(MONGO_DB)
-        .collection(MONGO_COLL_LOGS);
+        .collection(MONGO_COLLECTION);
 
-    match logs_collection.insert_one(doc! {"deviceId": &new_log.id, "poolName": &new_log.poolName, "sealedString": &new_log.sealedString, "createdOn": Bson::DateTime(Utc::now())}, None).await {
+    match data_collection.insert_one(doc! {"userId": &new_pool.id, "poolName": &new_pool.poolName, "sealedData": &new_pool.sealedData, "createdOn": Bson::DateTime(Utc::now())}, None).await {
         Ok(db_result) => {
             if let Some(new_id) = db_result.inserted_id.as_object_id() {
                 println!("New document inserted with id {}", new_id);   
--- rust-workspace/projects/data-server/src/main.rs
+++ rust-workspace/projects/data-server/src/main.rs
@@ -3,42 +3,22 @@ use actix_web::{web, App, HttpServer};
 use std::env;
 use std::sync::*;
 
-mod logs_handlers;
+mod data_handlers;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     // Create log for actix to output errors
     env::set_var("RUST_LOG", "actix_web=debug");
-    let mongo_url = env::var("CONNECTION_STRING_LOGS").unwrap();
+    // Remember to set connection string when starting the server
+    let mongo_url = env::var("CONNECTION_STRING").unwrap();
     let mut client_options = ClientOptions::parse(&mongo_url).await.unwrap();
     let client = web::Data::new(Mutex::new(Client::with_options(client_options).unwrap()));
     HttpServer::new(move || {
         App::new()
             .app_data(client.clone())
-            .service(web::scope("/api").configure(logs_handlers::scoped_config))
+            .service(web::scope("/api").configure(data_handlers::scoped_config))
     })
     .bind("127.0.0.1:8000")?
     .run()
     .await
 }
-
-// Upload data pool API //
-// Encryt from client to enclave - seal using enclave key - upload to Azure Blob (or Cosmos DB)
-// Data is uploaded in JSON format
-// Encrypt: Use SSL or TLS
-
-
-
-
-// Upload to Azure Blob storage or Cosmos DB - TBD //
-// https://github.com/Azure/azure-sdk-for-rust
-// https://crates.io/crates/azure_storage_blobs
-
-
-
-
-
-// Query data API //
-// Frontend must show which data packages the data creator has uploaded to the system
-// Option 1 - Query data service for list of data packages uploaded by the data creator
-// Option 2 - Lookup smart contracts associated with data creator's NTLS wallet
