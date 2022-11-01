7fa0869ef21b6ddda2e79463578f034851db05ee
binglekruger <bingkruger@hotmail.co.za>
2022-11-01 14:31:36 +0000

    refactor: remove unused imports + fix snake_case

--- rust-workspace/projects/data-server/src/data_handlers/mod.rs
+++ rust-workspace/projects/data-server/src/data_handlers/mod.rs
@@ -1,11 +1,10 @@
 use actix_web::{web, HttpResponse, Responder};
 use bson::{doc, Bson};
+use chrono::prelude::*;
 use futures::stream::StreamExt;
 use mongodb::{options::FindOptions, Client};
-use std::sync::Mutex;
-use chrono::prelude::*;
 use serde::Deserialize;
-use std::env;
+use std::sync::Mutex;
 
 #[derive(Deserialize)]
 pub struct UserData {
@@ -15,8 +14,8 @@ pub struct UserData {
 #[derive(Deserialize)]
 pub struct NewDataPool {
     pub id: String,
-    pub poolName: String, //User defined pool name
-    pub sealedData: String, //Update data format to match sealed data
+    pub pool_name: String,   //User defined pool name
+    pub sealed_data: String, //Update data format to match sealed data
 }
 
 // Mongo DB (CosmosDB) Name
@@ -33,15 +32,18 @@ pub fn scoped_config(cfg: &mut web::ServiceConfig) {
 }
 
 // This function accepts application data
-async fn get_data(data: web::Data<Mutex<Client>>, existingUser: web::Json<UserData>) -> impl Responder {
+async fn get_data(
+    data: web::Data<Mutex<Client>>,
+    existing_user: web::Json<UserData>,
+) -> impl Responder {
     let data_collection = data
         .lock()
         .unwrap()
         .database(MONGO_DB)
         .collection(MONGO_COLLECTION);
 
-    let userId = &existingUser.id;
-    let filter = doc! {"userId": userId};
+    let user_id = &existing_user.id;
+    let filter = doc! {"user_id": user_id};
     let find_options = FindOptions::builder().sort(doc! { "_id": -1}).build();
     let mut cursor = data_collection.find(filter, find_options).await.unwrap();
 
@@ -59,14 +61,17 @@ async fn get_data(data: web::Data<Mutex<Client>>, existingUser: web::Json<UserDa
     HttpResponse::Ok().json(results)
 }
 
-async fn add_data(data: web::Data<Mutex<Client>>, new_pool: web::Json<NewDataPool>) -> impl Responder {
+async fn add_data(
+    data: web::Data<Mutex<Client>>,
+    new_pool: web::Json<NewDataPool>,
+) -> impl Responder {
     let data_collection = data
         .lock()
         .unwrap()
         .database(MONGO_DB)
         .collection(MONGO_COLLECTION);
 
-    match data_collection.insert_one(doc! {"userId": &new_pool.id, "poolName": &new_pool.poolName, "sealedData": &new_pool.sealedData, "createdOn": Bson::DateTime(Utc::now())}, None).await {
+    match data_collection.insert_one(doc! {"user_id": &new_pool.id, "pool_name": &new_pool.pool_name, "sealed_data": &new_pool.sealed_data, "created_on": Bson::DateTime(Utc::now())}, None).await {
         Ok(db_result) => {
             if let Some(new_id) = db_result.inserted_id.as_object_id() {
                 println!("New document inserted with id {}", new_id);   
