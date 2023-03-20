6d80c77e6181fee0d535b0f8154973b4aed5800b
Bill Guo <bill@ntls.io>
2023-03-20 12:31:18 +0200

    refactor: change 'owner_name' to 'username' and remove phone_number

--- rust-sgx-workspace/projects/sgx-vault-test/enclave/src/vault_operations/test_create_vault.rs
+++ rust-sgx-workspace/projects/sgx-vault-test/enclave/src/vault_operations/test_create_vault.rs
@@ -7,20 +7,19 @@ use sgx_vault_impl::vault_operations::store::load_vault;
 
 pub(crate) fn create_vault_works() {
     let request = &actions::CreateVault {
-        owner_name: "New Owner".to_string(),
+        username: "New Username".to_string(),
         auth_password: "123456".to_string(),
-        phone_number: None,
     };
     let display = &match create_vault(request) {
         Result::Created(created) => created,
         Result::Failed(failed) => panic!("{}", failed),
     };
 
-    assert_eq!(display.owner_name, request.owner_name);
+    assert_eq!(display.username, request.username);
 
     let stored = load_vault(&display.vault_id).unwrap().unwrap();
     assert_eq!(display.vault_id, stored.vault_id);
-    assert_eq!(display.owner_name, stored.owner_name);
+    assert_eq!(display.username, stored.username);
     assert_eq!(
         display.algorand_address_base32,
         stored.algorand_account.address_base32()
